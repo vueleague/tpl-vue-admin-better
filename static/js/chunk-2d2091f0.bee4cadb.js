75decffe743634b5e6441b27a2b3364c0e4050b1
abelit <abelit@users.noreply.github.com>
2025-10-01 13:17:46 +0000

    deploy: 81efbbecfb7d3d42a8cbacea6eff49046242be32

--- static/js/chunk-2d2091f0.bee4cadb.js
+++
@@ -1,7 +0,0 @@
-/*!
- *  build: vue-admin-beautiful 
- *  vue-admin-beautiful author: chuzhixin 1204505056@qq.com 
- *  vue-admin-beautiful QQ Group(QQ群): 972435319、1139183756 
- *  time: 2021-12-24 12:43:47
- */
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2091f0"],{a869:function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"menu1-container"},[n("el-alert",{attrs:{closable:!1,title:"嵌套路由 1",type:"success"}},[n("router-view")],1)],1)},a=[],c=n("cba8"),l={},r=Object(c["a"])(l,s,a,!1,null,null,null);t["default"]=r.exports}}]);
